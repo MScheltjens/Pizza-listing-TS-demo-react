fa19153400f4622311edc4c8ecd986d32d06bf18
Mathias Scheltjens <mathias.scheltjens@gmail.com>
2022-08-31 14:28:02 +0200

    general state implemented

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FiShoppingCart } from "react-icons/fi";
 import CartCSS from "./Cart.module.css";
+import { AppStateContext } from "./AppState";
 
 interface Props {}
 
@@ -17,35 +18,46 @@ class Cart extends React.Component<Props, State> {
   }
 
   handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    if ((e.target as HTMLElement).nodeName === "SPAN") {
-      e.target as HTMLSpanElement;
-    }
+    // if ((e.target as HTMLElement).nodeName === 'SPAN') {
+    //   (e.target as HTMLSpanElement).;
+    // }
     this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
   };
 
   render() {
     return (
-      <div className={CartCSS.cartContainer}>
-        <button
-          className={CartCSS.button}
-          type="button"
-          onClick={this.handleClick}
-        >
-          <FiShoppingCart />
-          <span>2 pizza(s)</span>
-        </button>
-        <div
-          className={CartCSS.cartDropDown}
-          style={{
-            display: this.state.isOpen ? "block" : "none",
-          }}
-        >
-          <ul>
-            <li>Napoletana</li>
-            <li>Marinara</li>
-          </ul>
-        </div>
-      </div>
+      <AppStateContext.Consumer>
+        {(state) => {
+          return (
+            <div className={CartCSS.cartContainer}>
+              <button
+                className={CartCSS.button}
+                type="button"
+                onClick={this.handleClick}
+              >
+                <FiShoppingCart />
+                <span>{state.cart.items.length} pizza(s)</span>
+              </button>
+              <div
+                className={CartCSS.cartDropDown}
+                style={{
+                  display: this.state.isOpen ? "block" : "none",
+                }}
+              >
+                <ul>
+                  {state.cart.items.map((item) => {
+                    return (
+                      <li key={item.id}>
+                        {item.name} &times; {item.quantity}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          );
+        }}
+      </AppStateContext.Consumer>
     );
   }
 }
--- src/components/Pizza.tsx
+++ src/components/Pizza.tsx
@@ -1,4 +1,5 @@
-import PizzaCss from "./Pizza.module.css";
+import PizzaCSS from "./Pizza.module.css";
+import { AppSetStateContext, useSetState } from "./AppState";
 
 interface Pizza {
   id: number;
@@ -11,12 +12,43 @@ interface Props {
   pizza: Pizza;
 }
 
-const Pizza: React.FC<Props> = ({ pizza: { name, description, price } }) => {
+const Pizza: React.FC<Props> = ({ pizza }) => {
+  const setState = useSetState();
+  const handleAddToCartClick = () => {
+    setState((state) => {
+      const itemExists = state.cart.items.find((item) => item.id === pizza.id);
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          items: itemExists
+            ? state.cart.items.map((item) => {
+                if (item.id === pizza.id) {
+                  return { ...item, quantity: item.quantity + 1 };
+                }
+                return item;
+              })
+            : [
+                ...state.cart.items,
+                {
+                  id: pizza.id,
+                  name: pizza.name,
+                  price: pizza.price,
+                  quantity: 1,
+                },
+              ],
+        },
+      };
+    });
+  };
   return (
-    <li className={PizzaCss.container}>
-      <h2>{name}</h2>
-      <p>{description}</p>
-      <p>{price}</p>
+    <li className={PizzaCSS.container}>
+      <h2>{pizza.name}</h2>
+      <p>{pizza.description}</p>
+      <p>{pizza.price}</p>
+      <button type="button" onClick={handleAddToCartClick}>
+        Add to Cart
+      </button>
     </li>
   );
 };
