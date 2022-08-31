c445840f2b0a2a2d4bddf6f7d070c311c7dda482
Mathias Scheltjens <mathias.scheltjens@gmail.com>
2022-08-31 15:46:38 +0200

    specialOffer component

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import Cart from "./components/Cart";
 import AppCSS from "./App.module.css";
 import PizzaSVG from "./svg/pizza.svg";
 import AppStateProvider from "./components/AppState";
+import SpecialOffer from "./components/SpecialOffer";
 
 const App = () => {
+  const specialOfferPizza = pizzas.find((pizza) => pizza.specialOffer);
   return (
     <AppStateProvider>
       <div className={AppCSS.container}>
@@ -14,7 +16,8 @@ const App = () => {
           <div className={AppCSS.siteTitle}>Delicious Pizza</div>
           <Cart />
         </div>
-        <ul>
+        {specialOfferPizza && <SpecialOffer pizza={specialOfferPizza} />}
+        <ul className={AppCSS.pizzaList}>
           {pizzas.map((pizza) => {
             return <Pizza key={pizza.id} pizza={pizza} />;
           })}
