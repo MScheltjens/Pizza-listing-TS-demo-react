fa0cf4dd98563bc966cf653a0868a56fc9391bdb
Mathias Scheltjens <mathias.scheltjens@gmail.com>
2022-08-31 12:37:31 +0200

    added svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import pizzas from "./data/pizzas.json";
 import Pizza from "./components/Pizza";
 import AppCss from "../styles/App.module.css";
+import PizzaSVG from "./svg/pizza.svg";
 
 const App = () => {
   return (
     <div className={AppCss.container}>
+      <img src={PizzaSVG} alt="svg pizza logo" width={120} height={120} />
       <ul>
         {pizzas.map((pizza) => {
           return <Pizza key={pizza.id} pizza={pizza} />;
