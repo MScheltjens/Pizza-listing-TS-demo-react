2f3e87871ffd6c400447be34e817596bdf99a077
Mathias Scheltjens <mathias.scheltjens@gmail.com>
2022-08-31 12:40:23 +0200

    title added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import pizzas from "./data/pizzas.json";
 import Pizza from "./components/Pizza";
-import AppCss from "../styles/App.module.css";
+import AppCSS from "../styles/App.module.css";
 import PizzaSVG from "./svg/pizza.svg";
 
 const App = () => {
   return (
-    <div className={AppCss.container}>
-      <img src={PizzaSVG} alt="svg pizza logo" width={120} height={120} />
+    <div className={AppCSS.container}>
+      <div className={AppCSS.header}>
+        <img src={PizzaSVG} alt="svg pizza logo" width={120} height={120} />
+        <div className={AppCSS.siteTitle}>Delicious Pizza</div>
+      </div>
       <ul>
         {pizzas.map((pizza) => {
           return <Pizza key={pizza.id} pizza={pizza} />;
